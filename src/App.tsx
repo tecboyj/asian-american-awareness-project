c643aeeabd6fce4fa5b5406cfbe8165691d223ba
tecboyj <john.cromie@protonmail.com>
2026-06-08 16:20:20 -0400

    add website content

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
-import { Button } from "@/components/ui/button"
+import ReactMarkdown from "react-markdown"
+import reviewMd from "./assets/review.md?raw"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
-export function App() {
+export default function App() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
+    <div className="flex items-center justify-center gap-4 p-4">
+      <Card className="max-w-xl">
+        <CardHeader>
+          <CardTitle>Jade City Review</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ReactMarkdown>{reviewMd}</ReactMarkdown>
+        </CardContent>
+      </Card>
+      <Card>
+        <img
+          src="asian-american-awareness-project/fonda-lee.jpg"
+          alt="Fonda Lee"
+          className="flex w-full max-w-sm"
+        />
+        <CardHeader>
+          <CardTitle>Fonda Lee</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>
+            To learn more about the author, click{" "}
+            <a
+              href="https://www.fondalee.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 underline"
+            >
+              here
+            </a>
+          </p>
+        </CardContent>
+      </Card>
     </div>
   )
 }
-
-export default App
